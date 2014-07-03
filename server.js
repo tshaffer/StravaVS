2a1be7a4d8511a67c2541ef28a61dd2d6789b43b
Ted Shaffer <ted@brightsign.biz>
2014-07-03 13:01:06 -0700

    Tests using node as proxy server.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
@@ -46,19 +47,216 @@ var server = http.createServer(function(request, response) {
 
     if (request.url == '/') {
         filePath = 'public/index.html';
+        var absPath = './' + filePath;
+        serveStatic(response, cache, absPath);
+    } else if (request.url == '/showActivities.html')
+    {
+        showActivities(response);
     } else {
         //filePath = 'public' + request.url;
         //console.log("filePath = " + filePath);
         parsedUrl = url.parse(request.url);
         filePath = "public" + parsedUrl.pathname;
-     }
-    var absPath = './' + filePath;
-    serveStatic(response, cache, absPath);
+        var absPath = './' + filePath;
+        serveStatic(response, cache, absPath);
+    }
+    //var absPath = './' + filePath;
+    //serveStatic(response, cache, absPath);
 });
 
 server.listen(8080, function() {
   console.log("Server listening on port 8080.");
 });
 
+function showActivities(response) {
+    console.log('showActivities invoked');
+    //https.get("https://www.strava.com/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8", function (res) {
+    //    console.log("Got response: " + res.statusCode);
+    //}).on('error', function (e) {
+    //    console.log("Got error: " + e.message);
+    //});
+
+
+    // WORKS (both ways; athlete activities request and some other detailed info)
+    var options = {
+        host: 'www.strava.com',
+        //path: '/api/v3/activities/158581862',
+        path: '/api/v3/athlete/activities',
+        port: 443,
+        headers: {
+            'Authorization': 'Bearer ' + 'fb8085cc4c7f3633533e875eae3dc1e04cef06e8'
+        }
+    };
+
+    //https.get('https://www.strava.com/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8', function (res) {
+
+    str = ""
+
+    https.get(options, function (res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+        res.on('data', function (d) {
+            console.log("chunk received");
+            str += d;
+        });
+        res.on('end', function () {
+            console.log("end received");
+            //console.log(str);
+
+            activities = JSON.parse(str);
+            console.log(activities[0].id);
+
+            response.writeHead(
+                200,
+                { "content-type": 'application/json' }
+                );
+            //response.end(JSON.stringify({ activities: 1 }, null, 3));
+            response.end(JSON.stringify(activities, null, 3));
+        });
+
+    }).on('error', function () {
+        console.log('Caught exception: ' + err);
+    });
+
+
+
+
+    //str = ""
+
+    //var options = {
+    //    host: 'www.strava.com',
+    //    port: 443,
+    //    path: '/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8',
+    //    method: 'GET',
+    //    secureProtocol: 'SSLv3_method'
+    //};
+
+    //console.log('make request');
+    //var req = https.request(options, function (res) {
+    //    console.log('STATUS: ' + res.statusCode);
+    //    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    //    res.setEncoding('utf8');
+    //    res.on('error', function (err) {
+    //        console.log("got an error");
+    //    });
+    //    res.on('data', function (d) {
+    //        console.log("chunk received");
+    //        str += d;
+    //    });
+    //    res.on('end', function () {
+    //        console.log("end received");
+    //        //console.log(str);
+
+    //        activities = JSON.parse(str);
+    //        console.log(activities[0].id);
+    //    });
+
+    //    // write data to request body
+    //    req.write('data\n');
+    //    req.write('data\n');
+    //    req.end();
+    //}).on('error', function (err) {
+    //    //console.log("error");
+    //    console.log('Caught exception: ' + err);
+    //});
+
+
+
+
+    // the following worked (did not crash) but there's no way of knowing if the headers were added
+    //var options = {
+    //    host: 'www.strava.com',
+    //    port: 443,
+    //    path: '/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8',
+    //    headers: {
+    //        'pizza': 'tasty'
+    //    }
+    //};
+
+    //str = ""
+
+    //https.get(options, function(res) {
+    //    console.log('STATUS: ' + res.statusCode);
+    //    console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+    //    res.on('data', function (d) {
+    //        console.log("chunk received");
+    //        str += d;
+    //    });
+    //    res.on('end', function () {
+    //        console.log("end received");
+    //        //console.log(str);
+
+    //        activities = JSON.parse(str);
+    //        console.log(activities[0].id);
+    //    });
+
+    //}).on('error', function () {
+    //    console.log('Caught exception: ' + err);
+    //});
+
+
+
+
+    // WORKS
+    //str = ""
+
+    //https.get('https://www.strava.com/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8', function (res) {
+    //    console.log("statusCode: ", res.statusCode);
+    //    //console.log("headers: ", res.headers);
+
+    //    res.on('data', function (d) {
+    //        console.log("chunk received");
+    //        str += d;
+    //    });
+    //    res.on('end', function () {
+    //        console.log("end received");
+    //        //console.log(str);
+
+    //        activities = JSON.parse(str);
+    //        console.log(activities[0].id);
+    //    });
+
+    //}).on('end', function () {
+    //    console.log(str);
+    //});
+
+
+
+
+
+
+
+
+
+    //}).on('error', function (e) {
+    //    console.error(e);
+    //});
+
+    var str = '';
+
+    //var options = {
+    //    host: 'https://www.strava.com',
+    //    path: '/api/v3/athlete/activities?access_token=fb8085cc4c7f3633533e875eae3dc1e04cef06e8'
+    //};
+
+    //callback = function (response) {
+
+    //    response.on('data', function (chunk) {
+    //        console.log("chunk received");
+    //        str += chunk;
+    //    });
+
+    //    response.on('end', function () {
+    //        console.log("end received");
+    //        console.log(req.data);
+    //        console.log(str);
+    //        // your code here if you want to use the results !
+    //    });
+    //}
+
+    //var req = http.request(options, callback).end();
+}
 
 

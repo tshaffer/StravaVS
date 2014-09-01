2594317b9d34197305f53e7630e4bd8765005292
Ted Shaffer <ted@brightsign.biz>
2014-09-01 05:52:10 -0700

    point to AWS instance

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-﻿var hostName = "localHost";
-//var hostName = "ec2-54-183-201-187.us-west-1.compute.amazonaws.com";
+﻿//var hostName = "localHost";
+var hostName = "ec2-54-183-143-11.us-west-1.compute.amazonaws.com";
